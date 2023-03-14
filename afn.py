921e95c30da2e101d52a7e075fdd8bc19b9c7a42
Racso1624 <fer10_lb@outlook.com>
2023-03-14 09:46:15 -0600

    Se hace simulacion del afn

--- afn.py
+++ afn.py
@@ -306,8 +306,31 @@ class AFN(object):
         return transition_state_1, transition_state_2
     
 
-    def simulation(self, expression):
-        pass
+    # Se realiza la simulacion con el algoritmo del libro
+    def simulation(self, string):
+        # Se inicializan los estados con e_closure del inicial
+        states = self.e_closure(self.initial_state)
+        # Se inicia el conteo de caracteres de la cadena
+        character_count = 0
+
+        # Mientras hayan caracteres para verificar en el string
+        while(character_count < len(string)):
+            # Se toman los estados devueltos por e_closure del move con el caracter
+            states = self.e_closure(self.move(states, string[character_count]))
+            # Se pasa al siguiente caracter
+            character_count += 1
+
+        # Se hacen dos sets para lograr hacer operaciones de conjuntos entre ellos
+        set_states = set(states)
+        set_final_states = set(self.final_state)
+
+        # Se verifica que los estados encontrados se encuentren en el conjunto de estados finales
+        if(set_states.intersection(set_final_states).__len__() != 0):
+            return "Cadena Aceptada"
+        else:
+            return "Cadena No Aceptada"
+        
+
     
     # Se utiliza el algoritmo del libro para calcular e-closure
     def e_closure(self, states):
@@ -331,7 +354,29 @@ class AFN(object):
         
         # Se retorna el resultado
         return states_result
+    
+    # Se utiliza el algoritmo de e-closure para calcular move
+    def move(self, states, character):
+        # Se inicia el stack con los estados de T
+        states_stack = list(states)
+        # Se inicia sin estados
+        states_result = []
 
+        # Se itera mientra el stack no se encuentre vacio
+        while(len(states_stack) != 0):
+            # Se saca el estado t
+            state = states_stack.pop()
+            # Se revisa en cada transicion
+            for i in self.transitions:
+                # Se revisa que tenga transicion con ε 
+                if(i[0] == state and i[1] == character):
+                    # Si el estado no esta en los resultados se ingresa
+                    if(i[2] not in states_result):
+                        states_result.append(i[2])
+                        states_stack.append(i[2])
+        
+        # Se retorna el resultado
+        return states_result
 
     # Funcion realizada para ordenar las transiciones de manera que se puedan visualizar
     # con un orden de 1 a N estados
