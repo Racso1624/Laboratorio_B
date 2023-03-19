31b0866f1666187f3b29561f5543979d63bf93d2
Racso1624 <fer10_lb@outlook.com>
2023-03-19 00:30:29 -0600

    Se crea first y last pos

--- afd_construction.py
+++ afd_construction.py
@@ -8,3 +8,42 @@ def nullable(node):
         return (nullable(node.left_child) or nullable(node.right_child))
     elif(node.character == "."):
         return (nullable(node.left_child) and nullable(node.right_child))
+    elif(node.character == "+"):
+        return nullable(node.left_child)
+    else:
+        return False
+    
+def firstpos(node):
+
+    if(node.character == "ε"):
+        return set()
+    elif(node.character == "|"):
+        return (firstpos(node.right_child).union(node.left_child))
+    elif(node.character == "."):
+        if(nullable(node.left_child)):
+            return (firstpos(node.right_child).union(node.left_child))
+        else:
+            return firstpos(node.left_child)
+    elif(node.character in "*+?"):
+        return firstpos(node.left_child)
+    else:
+        return {node.position}
+    
+def lastpos(node):
+
+    if(node.character == "ε"):
+        return set()
+    elif(node.character == "|"):
+        return (firstpos(node.right_child).union(node.left_child))
+    elif(node.character == "."):
+        if(nullable(node.right_child)):
+            return (firstpos(node.right_child).union(node.left_child))
+        else:
+            return firstpos(node.right_child)
+    elif(node.character in "*+?"):
+        return firstpos(node.left_child)
+    else:
+        return {node.position}
+    
+def followpos(node):
+    pass
