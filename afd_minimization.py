a7f54a906939d2b02b7390c78fc01fc45dac883f
Racso1624 <fer10_lb@outlook.com>
2023-03-19 21:31:48 -0600

    Se agregan parte del algoritmo de minimizacion

--- afd_minimization.py
+++ afd_minimization.py
@@ -13,17 +13,24 @@ def afdMinimization(afd):
         else:
             normal_state.append(i)
 
-    if(len(acceptance_state) != 0):
-        partitions.append(acceptance_state)
     if(len(normal_state) != 0):
         partitions.append(normal_state)
+    if(len(acceptance_state) != 0):
+        partitions.append(acceptance_state)
 
     not_distinguishable = True
     while(not_distinguishable):
+        partitions_table = []
         for partition in partitions:
-            for state in partition:
+            for state in states:
                 for symbol in symbols:
                     for transition in transitions:
                         if(transition[0] == state and transition[1] == symbol):
-                            print(transition)
+                            for partition_2 in partitions:
+                                if(transition[2] in partition_2):
+                                    partitions_table.append([state, symbol, partitions.index(partition_2), partitions.index(partition)])
+
+        new_partition = []
+        for partition in partitions:
+            pass                        
         not_distinguishable = False
